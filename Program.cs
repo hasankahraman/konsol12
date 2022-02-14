677f300718c30d9174c00a503db820ab9aee8f92
Hasan KAHRAMAN <hasankahraman06092@gmail.com>
2022-02-14 17:20:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// erişim_belirteci geri_dönüş_tipi metot_adı(parametreler) {
+
+    //komutlar
+
+    //return
+
+// }
+
+int a = 2;
+int b = 3;
+
+Console.WriteLine(a+b);
+Console.ReadLine();
+
+
+int Topla (int a, int b){
+
+    return a+b;
+}
+
+
+Console.WriteLine(Topla(a,b));
+Console.ReadLine();
